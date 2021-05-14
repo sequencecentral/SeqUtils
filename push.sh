1dfda0fd35d662842fbc77ca99a61ad1447d927c
Steve Ayers <californiastephen@outlook.com>
2021-05-14 09:40:51 -0700

    pypi upload

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Github
+git add --all
+git commit -am 'pypi upload'
+git push
+
+#make an egg
+python setup.py bdist_egg
+
+#make pypi dist
+python setup.py sdist
+
+#upload pypi
+pip install twine
+twine upload -u sequencecentral --skip-existing dist/*
