39eed44d217ba96c92cf5bee1accb36a7afccc43
Steve Ayers <californiastephen@outlook.com>
2021-05-17 14:44:21 -0700

    pypi upload

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='Steve Ayers, Ph.D.',
     author_email='steve@sequenccecentral.com',
     # install_requires=[],
-    version='1.0.10',
+    version='1.0.11',
     license='MIT',
     # packages=['synchronicity','synchronicity.quotewidget'],
     # packages = find_packages(),
@@ -33,7 +33,8 @@ setup(
     # install_requires=[''],
     # *strongly* suggested for sharing
     long_description=open('README.md').read(),
-    install_requires=['bleach==3.3.0', 'certifi==2020.12.5', 'chardet==4.0.0', 'colorama==0.4.4', 'docutils==0.17.1', 'idna==2.10', 'importlib-metadata==4.0.1', 'keyring==23.0.1', 'packaging==20.9', 'pkginfo==1.7.0', 'Pygments==2.9.0', 'pyparsing==2.4.7', 'readme-renderer==29.0', 'requests==2.25.1', 'requests-toolbelt==0.9.1', 'rfc3986==1.5.0', 'six==1.16.0', 'tqdm==4.60.0', 'twine==3.4.1', 'urllib3==1.26.4', 'webencodings==0.5.1', 'zipp==3.4.1'],
+    # install_requires=['bleach==3.3.0', 'certifi==2020.12.5', 'chardet==4.0.0', 'colorama==0.4.4', 'docutils==0.17.1', 'idna==2.10', 'importlib-metadata==4.0.1', 'keyring==23.0.1', 'packaging==20.9', 'pkginfo==1.7.0', 'Pygments==2.9.0', 'pyparsing==2.4.7', 'readme-renderer==29.0', 'requests==2.25.1', 'requests-toolbelt==0.9.1', 'rfc3986==1.5.0', 'six==1.16.0', 'tqdm==4.60.0', 'twine==3.4.1', 'urllib3==1.26.4', 'webencodings==0.5.1', 'zipp==3.4.1'],
+    install_requires=['bleach==3.3.0', 'certifi==2020.12.5', 'chardet==4.0.0', 'colorama==0.4.4', 'docutils==0.17.1', 'environ==1.0', 'idna==2.10', 'importlib-metadata==4.0.1', 'keyring==23.0.1', 'packaging==20.9', 'pkginfo==1.7.0', 'Pygments==2.9.0', 'pyparsing==2.4.7', 'readme-renderer==29.0', 'requests==2.25.1', 'requests-toolbelt==0.9.1', 'rfc3986==1.5.0', 'six==1.16.0', 'tqdm==4.60.0', 'twine==3.4.1', 'urllib3==1.26.4', 'webencodings==0.5.1', 'zipp==3.4.1'],
     # install_requires=open('requirements.txt').read(), #['click==7.1.2','joblib==1.0.1','nltk==3.6.1','regex==2021.4.4','tqdm==4.60.0'],
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
